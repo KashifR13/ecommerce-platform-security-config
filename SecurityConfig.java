65ae700f5949777213f0ac0ae418e1452d4e9b51
KASHIF RASOOL <kashifrasool.13@gmail.com>
2024-10-21 01:40:21 +0300

    Update SecurityConfig.java

--- SecurityConfig.java
+++ SecurityConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,15 +16,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        // TODO: Implement the security configuration once the application is tested and ready
-
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
-                        .anyRequest().permitAll() // Allow all requests
-                );
+                        .requestMatchers("/public/**").permitAll() // Allow public access to certain endpoints
+                        .anyRequest().authenticated() // Require authentication for other requests
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll) // Enable form login
+                .logout(LogoutConfigurer::permitAll); // Enable logout
 
         return http.build();
     }
-
+    
 }
